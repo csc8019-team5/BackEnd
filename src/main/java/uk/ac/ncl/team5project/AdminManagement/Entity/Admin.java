0098c1c559bfc79d07552775ab270cc9962f3212
Qingyu <q.cao4@newcastle.ac.uk>
2025-04-02 17:59:13 +0100

    Admin

---
+++ src/main/java/uk/ac/ncl/team5project/AdminManagement/Entity/Admin.java
@@ -0,0 +1,5 @@
+package uk.ac.ncl.team5project.AdminManagement.Entity;
+
+public class Admin {
+    
+}
---
+++ src/main/java/uk/ac/ncl/team5project/AdminManagement/Service/AdminService.java
@@ -0,0 +1,5 @@
+package uk.ac.ncl.team5project.AdminManagement.Service;
+
+public class AdminService {
+    
+}
