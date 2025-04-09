4b1ec4d906b6d73f921ad84300683af580ae61b1
Qingyu <q.cao4@newcastle.ac.uk>
2025-04-09 15:29:22 +0100

    Configuration

--- src/main/java/uk/ac/ncl/team5project/com/admin/controller/AdminController.java
+++ src/main/java/uk/ac/ncl/team5project/com/admin/controller/AdminController.java
@@ -1,5 +1,39 @@
 package uk.ac.ncl.team5project.com.admin.controller;
+/**
+ * @file AdminController.java
+ * @date 2025-04-09 02:58
+ * @function_description: 
+ * @interface_description: 
+ *     @calling_sequence: 
+ *     @arguments_description: 
+ *     @list_of_subordinate_classes: 
+ * @discussion: 
+ * @development_history: 
+ *     @designer USER 
+ *     @reviewer: 
+ *     @review_date: 
+ *     @modification_date: 
+ *     @description: 
+ */
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public class AdminController {
+
+//METHOD 1:  Qurey and display all administors from database
+    public List<Admin> display(){
+
+        return null;
+    }
+
+//METHOD 2: Add a new Administor
+    public void insert(){
+
+    }
 
 }
--- src/main/java/uk/ac/ncl/team5project/com/admin/entity/Admin.java
+++ src/main/java/uk/ac/ncl/team5project/com/admin/entity/Admin.java
@@ -1,5 +1,36 @@
 package uk.ac.ncl.team5project.com.admin.entity;
+/**
+ * @file Admin.java
+ * @date 2025-04-09 02:58
+ * @function_description: 
+ * @interface_description: 
+ *     @calling_sequence: 
+ *     @arguments_description: 
+ *     @list_of_subordinate_classes: 
+ * @discussion: 
+ * @development_history: 
+ *     @designer USER 
+ *     @reviewer: 
+ *     @review_date: 
+ *     @modification_date: 
+ *     @description: 
+ */
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Admin {
-    
+    Integer adminId;
+    String adminName;
+    String adminEmail;
+    String password;
+    Integer adminLevel;
+    LocalDate registrationTime;
+    LocalDate updateTime;
 }
