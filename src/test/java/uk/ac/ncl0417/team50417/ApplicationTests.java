8b2c4cd6f1d652bdcc71d656f3b0c09f06058c18
yaomenghui <c4051741@newcastle.ac.uk>
2025-04-26 20:32:02 +0100

    Add files via upload

---
+++ src/test/java/uk/ac/ncl0417/team50417/ApplicationTests.java
@@ -0,0 +1,12 @@
+package uk.ac.ncl0417.team50417;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
